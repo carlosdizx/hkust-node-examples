3450b8218d9258e7944364bcd790dea5d8346028
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-05 11:21:32 -0500

    :sparkles: create example for test when run app

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-const rect = {
-  perimeter: (x: number, y: number) => 2 * (x + y),
-  area: (x: number, y: number) => x * y
-};
-
-const solveRectangle = (l: number, b: number) => {
-  console.log("Solving rectangle with l=" + l + " and b=" + b);
-
-  if (l <= 0 || b <= 0)
-    return console.warn("Rectangle dimensions should be greater than zero");
-  else {
-    console.log("The area rectangle is", rect.area(l, b));
-    console.log("The permiter rectangle is", rect.perimeter(l, b));
-  }
-};
+import solveRectangle  from "./rectangle";
 
 solveRectangle(2, 4);
 solveRectangle(3, 5);
 solveRectangle(0, 5);
 solveRectangle(-3, 5);
-
