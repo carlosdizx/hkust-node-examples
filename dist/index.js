95eb2faae5ec35c8b9af4b75b49d1553e09bb76d
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-05 11:14:57 -0500

    :sparkles: create example for test when run app

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,18 @@
 "use strict";
 const rect = {
     perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
 };
-console.log(rect.perimeter(2, 3));
+const solveRectangle = (l, b) => {
+    console.log("Solving rectangle with l=" + l + " and b=" + b);
+    if (l <= 0 || b <= 0)
+        return console.warn("Rectangle dimensions should be greater than zero");
+    else {
+        console.log("The area rectangle is", rect.area(l, b));
+        console.log("The permiter rectangle is", rect.perimeter(l, b));
+    }
+};
+solveRectangle(2, 4);
+solveRectangle(3, 5);
+solveRectangle(0, 5);
+solveRectangle(-3, 5);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 const rect = {
   perimeter: (x: number, y: number) => 2 * (x + y),
+  area: (x: number, y: number) => x * y
 };
 
-console.log(rect.perimeter(2, 3));
+const solveRectangle = (l: number, b: number) => {
+  console.log("Solving rectangle with l=" + l + " and b=" + b);
+
+  if (l <= 0 || b <= 0)
+    return console.warn("Rectangle dimensions should be greater than zero");
+  else {
+    console.log("The area rectangle is", rect.area(l, b));
+    console.log("The permiter rectangle is", rect.perimeter(l, b));
+  }
+};
+
+solveRectangle(2, 4);
+solveRectangle(3, 5);
+solveRectangle(0, 5);
+solveRectangle(-3, 5);
+
