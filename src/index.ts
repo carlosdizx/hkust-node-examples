21b2b4342474330ea92cea75dbf8d48cfabe1121
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-05 12:55:06 -0500

    :sparkles: usage callback functions and set timeout for delay example

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import solveRectangle  from "./rectangle";
+import rectangle from "./rectangle";
+
+const solveRectangle = (l: number, b: number) => {
+  rectangle(l, b, (error: any, rectangle: any) => {
+    if (error) console.error(error.message);
+    else {
+      console.log("The area rectangle is", rectangle.area(l, b));
+      console.log("The permiter rectangle is", rectangle.perimeter(l, b));
+    }
+  });
+};
 
 solveRectangle(2, 4);
 solveRectangle(3, 5);
--- src/rectangle.ts
+++ src/rectangle.ts
@@ -1,15 +1,17 @@
 const perimeter = (x: number, y: number) => 2 * (x + y);
-const area = (x: number, y: number) => 2 * (x + y);
+const area = (x: number, y: number) => x * y;
 
-const solveRectangle = (l: number, b: number) => {
-  console.log("Solving rectangle with l=", l , " and b=", b);
-
+const rectangle = (l: number, b: number, callback: Function) => {
   if (l <= 0 || b <= 0)
-    return console.warn("Rectangle dimensions should be greater than zero");
-  else {
-    console.log("The area rectangle is", area(l, b));
-    console.log("The permiter rectangle is", perimeter(l, b));
-  }
+    setTimeout(
+      () =>
+        callback(
+          new Error("Rectangle dimensions should be greater than zero"),
+          null
+        ),
+      2000
+    );
+  else setTimeout(() => callback(null, { perimeter, area }), 2000);
 };
 
-export default solveRectangle;
+export default rectangle;
