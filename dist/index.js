05c229d5f8ab22e19c13b45debd598d79a6ea2b5
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-05 11:31:13 -0500

    :no_see_evil: ignore files in dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const rectangle_1 = require("./rectangle");
-(0, rectangle_1.default)(2, 4);
-(0, rectangle_1.default)(3, 5);
-(0, rectangle_1.default)(0, 5);
-(0, rectangle_1.default)(-3, 5);
