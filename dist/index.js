6ac87c652cb63cedd254c6502bf6be13490eb871
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-05 10:55:45 -0500

    :tada: configure project for run app with tsc and compile and only script

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+};
+console.log(rect.perimeter(2, 3));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const rect = {
+  perimeter: (x: number, y: number) => 2 * (x + y),
+};
+
+console.log(rect.perimeter(2, 3));
